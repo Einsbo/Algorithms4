13c837300c0bb9ead9efecccdc6ce0b5c7e68e3c
bakashinji-hasse <m18753168203@gmail.com>
2023-09-06 23:06:54 +0800

    23:04 2023/9/6

--- src/C1_Fundamentals/C1_1_ProgrammingModel/BinarySearch.java
+++ src/C1_Fundamentals/C1_1_ProgrammingModel/BinarySearch.java
@@ -1,3 +1,5 @@
+package C1_1_ProgrammingModel;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -22,13 +24,14 @@ public class BinarySearch {
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) hi = mid + 1;
+            else if (key > a[mid]) lo = mid + 1;
             else                   return mid;
         }
         return -1;
     }
 
     public static void main(String[] args) {
+//        StdOut.println(args[0][0]);
         int[] whitelist = In.readInts(args[0]);
         Arrays.sort(whitelist);
         while (!StdIn.isEmpty()) {
